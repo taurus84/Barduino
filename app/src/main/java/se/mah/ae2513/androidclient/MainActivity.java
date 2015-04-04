ddf3b5d534526381a0f56322997835f50175f938
taurus84 <David@Davids-iMac.local>
2015-04-04 22:43:15 +0200

    Extentions
    AsyncTask, sending and receiving from server.

--- app/src/main/java/se/mah/ae2513/androidclient/MainActivity.java
+++ app/src/main/java/se/mah/ae2513/androidclient/MainActivity.java
@@ -1,10 +1,13 @@
 package se.mah.ae2513.androidclient;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,14 +17,17 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 
+
+
 /**
  * The class creates a connection to a server where user chooses
  * ip-address and port number of the server.
  */
 public class MainActivity extends ActionBarActivity {
 
-    private Button connectButton;
-    private EditText etIP, etPort;
+    private Button connectButton, send;
+    private EditText etIP, etPort, stringText;
+    private TCPClient mTcpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +35,91 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         setComponents();
+      //  connectToServer();
         setupConnectButton();
+
+
     }
 
+    /**
+     * *** Not used ***
+     */
+    private void connectToServer() {
+        try {
+            Socket socket = new Socket("192.168.1.2", 4444);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Setting up buttons and editTexts
+     */
     private void setComponents() {
         etIP = (EditText) findViewById(R.id.textIP);
         etPort = (EditText) findViewById(R.id.textPort);
+        send = (Button) findViewById(R.id.btnSend);
+        stringText = (EditText) findViewById(R.id.editText);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get text from stringText and send it to server
+                String message = stringText.getText().toString();
+                if (mTcpClient != null) {
+                    mTcpClient.sendMessage(message);
+                    stringText.setText("");
+                }
+            }
+        });
+
+
     }
 
     private void setupConnectButton() {
         Button connectButton = (Button) findViewById(R.id.btnConnect);
         connectButton.setOnClickListener(new View.OnClickListener() {
 
-
             @Override
             public void onClick(View v) {
-
-                String ipNbr = etIP.getText().toString();
-                int port = Integer.parseInt(etPort.getText().toString());
-                Thread thread = new Thread(new TCPClient(ipNbr, port));
-                thread.start();
+                new connectTask().execute("");
+                //String ipNbr = etIP.getText().toString();
+                //int port = Integer.parseInt(etPort.getText().toString());
+                //Thread thread = new Thread(new TCPClient(ipNbr, port));
+                //thread.start();
+                //Log.i("Testing", "Det funkar kanske");
 
             }
         });
+    }
+
+
+
+    /**
+     * aSyncTask to be used to perform actions in background while letting
+     * the main activity run on foreground. In this example, the background
+     * activity is the connection to the server.
+     *
+     * Three generics in interface AsyncTask
+     * 1. Type of references passed to doInBackGround
+     * 2. Type of reference passed to onProgressUpdate
+     * 3. Type of reference returned by doInBackground
+     */
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... params) {
+            mTcpClient = new TCPClient(etIP.getText().toString(), Integer.parseInt(etPort.getText().toString()), new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run();
+
+            return null;
+        }
     }
 
 
--- app/src/main/java/se/mah/ae2513/androidclient/TCPClient.java
+++ app/src/main/java/se/mah/ae2513/androidclient/TCPClient.java
@@ -1,5 +1,6 @@
 package se.mah.ae2513.androidclient;
 
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.*;
@@ -13,26 +14,76 @@ public class TCPClient implements Runnable {
 
     private String ipNumber;
     private int port;
+    PrintWriter out;
+    BufferedReader in;
+    PrintStream ps;
+    InputStreamReader ir;
+    private String serverMessage;
+    private OnMessageReceived mMessageListener = null;
+    private boolean mRun = false;
 
-    public TCPClient(String ipNumber, int port) {
+
+    public TCPClient(String ipNumber, int port, OnMessageReceived listener ) {
         this.ipNumber = ipNumber;
         this.port = port;
+        mMessageListener = listener;
+
     }
 
-    public void run() {
-        try {
-            Socket socket = new Socket("192.168.1.2", 4444);
-            PrintStream ps = new PrintStream(socket.getOutputStream());
-            ps.println("Hello server!");
-            InputStreamReader ir = new InputStreamReader(socket.getInputStream());
-            BufferedReader br = new BufferedReader(ir);
-            String message = br.readLine();
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * Sends the message entered by client to the server
+     * @param message text entered by client
+     */
+    public void sendMessage(String message){
+        if (out != null && !out.checkError()) {
+            out.println(message);
+            //Prints out the message on consol in debugging perpose
+            Log.i("MESSAGE TO SERVER", message);
+            out.flush();
         }
     }
 
+    public void run() {
+        mRun = true;
+        try {
+            //create a socket to make the connection with the server
+            Socket socket = new Socket(ipNumber, port);
+            /*PrintStream ps = new PrintStream(socket.getOutputStream());
+            ps.println("Hello server!");
+            InputStreamReader ir = new InputStreamReader(socket.getInputStream());
+            in = new BufferedReader(ir);
+            String message = in.readLine();
+            */
+
+            try {
+                //send the message to the server
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                //receive the message which the server sends back
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                //in this while the client listens for the messages sent by the server
+                while (mRun) {
+                    serverMessage = in.readLine();
+
+                    if (serverMessage != null && mMessageListener != null) {
+                        //call the method messageReceived from MyActivity class
+                        mMessageListener.messageReceived(serverMessage);
+                        //Prints out the message on consol in debugging perpose
+                        Log.i("RESPONSE FROM SERVER", "S: Received Message: '" + serverMessage + "'");
+                    }
+                    serverMessage = null;
+
+                }
+            } catch (Exception e) {
+                Log.e("TCP", "S: Error", e);
+            }
+
+        } catch (IOException e) {
+            Log.e("TCP", "C: Error", e);
+        }
+    }
+
+    public interface OnMessageReceived {
+        public void messageReceived(String message);
+    }
 }
