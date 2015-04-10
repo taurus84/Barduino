fa8d61d172c91afcad450a47edeb155baaf41550
Rhygar <john_tengvall@hotmail.com>
2015-04-10 10:59:56 +0200

    Logiken bakom Seekbars

--- app/src/main/java/se/mah/ae2513/androidclient/Fragment_Start.java
+++ app/src/main/java/se/mah/ae2513/androidclient/Fragment_Start.java
@@ -5,16 +5,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by John on 15-04-08.
  */
-public class Fragment_Start extends Fragment {
+public class Fragment_Start extends Fragment implements SeekBar.OnSeekBarChangeListener {
+    private SeekBar seekBar1, seekBar2, seekBar3, seekBar4;
+    private TextView cl1,cl2,cl3,cl4;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.start_layout, container, false);
         return v;
+
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        seekBar1 = (SeekBar) getActivity().findViewById(R.id.seekBar);
+        seekBar2 = (SeekBar) getActivity().findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar) getActivity().findViewById(R.id.seekBar3);
+        seekBar4 = (SeekBar) getActivity().findViewById(R.id.seekBar4);
+
+        seekBar1.setOnSeekBarChangeListener(this);
+        seekBar2.setOnSeekBarChangeListener(this);
+        seekBar3.setOnSeekBarChangeListener(this);
+        seekBar4.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        cl1 = (TextView) getActivity().findViewById(R.id.cl1);
+        cl1.setText("CL: " + seekBar1.getProgress());
+
+        cl2 = (TextView) getActivity().findViewById(R.id.cl2);
+        cl2.setText("CL: " + seekBar2.getProgress());
+
+        cl3 = (TextView) getActivity().findViewById(R.id.cl3);
+        cl3.setText("CL: " + seekBar3.getProgress());
+
+        cl4 = (TextView) getActivity().findViewById(R.id.cl4);
+        cl4.setText("CL: " + seekBar4.getProgress());
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
