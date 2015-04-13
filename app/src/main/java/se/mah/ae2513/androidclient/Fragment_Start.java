5ab8150a32a9e5b20d0256fbe964899b39c7cd6c
Rhygar <john_tengvall@hotmail.com>
2015-04-13 14:18:41 +0200

    Ändring text Activitybar

--- app/src/main/java/se/mah/ae2513/androidclient/Fragment_Start.java
+++ app/src/main/java/se/mah/ae2513/androidclient/Fragment_Start.java
@@ -8,59 +8,142 @@ import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by John on 15-04-08.
  */
-public class Fragment_Start extends Fragment implements SeekBar.OnSeekBarChangeListener {
+public class Fragment_Start extends Fragment {
     private SeekBar seekBar1, seekBar2, seekBar3, seekBar4;
     private TextView cl1,cl2,cl3,cl4;
+    private int seekbarMax = 25;
+    private ArrayList <SeekBar> seekBarList = new ArrayList<SeekBar>();
+    private ArrayList <TextView> textViewList = new ArrayList<TextView>();
+    private ArrayList <TextView> ratioLbls = new ArrayList<TextView>();
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.start_layout, container, false);
         return v;
-
-
     }
 
-    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        SeekBarsListener listener = new SeekBarsListener();
         seekBar1 = (SeekBar) getActivity().findViewById(R.id.seekBar1);
         seekBar2 = (SeekBar) getActivity().findViewById(R.id.seekBar2);
         seekBar3 = (SeekBar) getActivity().findViewById(R.id.seekBar3);
         seekBar4 = (SeekBar) getActivity().findViewById(R.id.seekBar4);
+        seekBarList.add(seekBar1);
+        seekBarList.add(seekBar2);
+        seekBarList.add(seekBar3);
+        seekBarList.add(seekBar4);
 
-        seekBar1.setOnSeekBarChangeListener(this);
-        seekBar2.setOnSeekBarChangeListener(this);
-        seekBar3.setOnSeekBarChangeListener(this);
-        seekBar4.setOnSeekBarChangeListener(this);
+        textViewList.add(cl1);
+        textViewList.add(cl2);
+        textViewList.add(cl3);
+        textViewList.add(cl4);
+
+        seekBar1.setOnSeekBarChangeListener(listener);
+        seekBar2.setOnSeekBarChangeListener(listener);
+        seekBar3.setOnSeekBarChangeListener(listener);
+        seekBar4.setOnSeekBarChangeListener(listener);
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        cl1 = (TextView) getActivity().findViewById(R.id.cl1);
-        cl1.setText(seekBar1.getProgress() + " cl");
+    public class SeekBarsListener implements SeekBar.OnSeekBarChangeListener{
 
-        cl2 = (TextView) getActivity().findViewById(R.id.cl2);
-        cl2.setText(seekBar2.getProgress() + " cl");
+        private void updateValues(Object object) {
+            int sumSliders = 0;
+            Iterator<TextView> iterLabel;
+            Iterator<SeekBar> iterSlider = seekBarList.iterator();
+            SeekBar tempSlider;
+            TextView tempLabel;
 
-        cl3 = (TextView) getActivity().findViewById(R.id.cl3);
-        cl3.setText(seekBar3.getProgress() + " cl");
+            while (iterSlider.hasNext()) {
+                tempSlider = iterSlider.next();
+                tempSlider.setMax(seekbarMax);
+                sumSliders += tempSlider.getProgress();
+            }
 
-        cl4 = (TextView) getActivity().findViewById(R.id.cl4);
-        cl4.setText(seekBar4.getProgress() + " cl");
+            iterSlider = seekBarList.iterator();
+            iterLabel = textViewList.iterator();
+            int sliderValue;
+            double percentageDouble;
+            int percentage;
 
-    }
+            if (sumSliders > seekbarMax) {
+                int decreaseSum = sumSliders - seekbarMax;
+                int nbrOfSlidersToShare = 0;
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+                while (iterSlider.hasNext()) {
+                    tempSlider = iterSlider.next();
+                    if (object != tempSlider) {
+                        if (tempSlider.getProgress() != 0) {
+                            nbrOfSlidersToShare++;
+                        }
+                    }
+                }
 
-    }
+                iterSlider = seekBarList.iterator();
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+                while (iterSlider.hasNext()) {
+                    tempSlider = iterSlider.next();
+                    if (object != tempSlider) {
+                        if (tempSlider.getProgress() < (decreaseSum / nbrOfSlidersToShare)) {
+                            decreaseSum -= tempSlider.getProgress();
+                            tempSlider.setProgress(0);
+                        } else {
+                            tempSlider.setProgress(tempSlider.getProgress()
+                                    - (decreaseSum / nbrOfSlidersToShare));
+                        }
+                    }
+                }
 
+            }
+
+            iterSlider = seekBarList.iterator();
+
+            while (iterSlider.hasNext()) {
+                tempLabel = iterLabel.next();
+                sliderValue = iterSlider.next().getProgress();
+                percentageDouble = ((double) sliderValue / sumSliders) * seekbarMax;
+                percentage = (int) percentageDouble;
+                if (percentageDouble - percentage > 0.5)
+                    percentage++;
+
+                tempLabel.setText(percentage + " cl");
+            }
+        }
+
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            updateValues(seekBar);
+            //cl1 = (TextView) getActivity().findViewById(R.id.cl1);
+            //cl1.setText(seekBar1.getProgress() + " cl");
+
+            //cl2 = (TextView) getActivity().findViewById(R.id.cl2);
+            //cl2.setText(seekBar2.getProgress() + " cl");
+
+            //cl3 = (TextView) getActivity().findViewById(R.id.cl3);
+            //cl3.setText(seekBar3.getProgress() + " cl");
+
+            //cl4 = (TextView) getActivity().findViewById(R.id.cl4);
+            //cl4.setText(seekBar4.getProgress() + " cl");
+
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
     }
 }
